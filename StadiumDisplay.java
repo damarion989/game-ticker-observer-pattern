070cfa5285c13fa164f414e62b9854ff50816493
damarion989 <d.butlers282@gmail.com>
2026-08-30 20:20:36 -0500

    Update StadiumDisplay.java

--- StadiumDisplay.java
+++ StadiumDisplay.java
@@ -1,1 +1,12 @@
+public class StadiumDisplay implements Observer {
+    private GameTicker ticker;
 
+    public StadiumDisplay(GameTicker ticker) {
+        this.ticker = ticker;
+    }
+
+    @Override
+    public void update() {
+        System.out.println("SCREEN UPDATE: " + ticker.getLatestUpdate());
+    }
+}
