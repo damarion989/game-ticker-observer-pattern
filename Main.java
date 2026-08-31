6c68ab82fe8d5c6fa2656aebc23fec5d087f89ea
damarion989 <d.butlers282@gmail.com>
2026-08-30 22:19:24 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        GameTicker ticker = new GameTicker();
 
+        MobilePushNotification pushNotification = new MobilePushNotification(ticker);
+        StadiumDisplay stadiumDisplay = new StadiumDisplay(ticker);
+        SocialMediaBot socialMediaBot = new SocialMediaBot(ticker);
+
+        ticker.register(pushNotification);
+        ticker.register(stadiumDisplay);
+        ticker.register(socialMediaBot);
+
+        System.out.println("--- Simulating Game Event ---");
+        ticker.addUpdate("Touchdown! Home team scores!");
+
+        System.out.println("\n--- Another Update ---");
+        ticker.addUpdate("Halftime score: 21-14");
+    }
+}
