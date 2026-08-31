661c7160d042cad1a6862f32a8cfc605a0531613
damarion989 <d.butlers282@gmail.com>
2026-08-30 20:20:07 -0500

    Implement MobilePushNotification as an Observer

--- MobilePushNotification.java
+++ MobilePushNotification.java
@@ -1,1 +1,12 @@
+public class MobilePushNotification implements Observer {
+    private GameTicker ticker;
 
+    public MobilePushNotification(GameTicker ticker) {
+        this.ticker = ticker;
+    }
+
+    @Override
+    public void update() {
+        System.out.println("PUSH ALERT: " + ticker.getLatestUpdate());
+    }
+}
