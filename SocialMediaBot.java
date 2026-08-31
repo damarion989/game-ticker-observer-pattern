f0e53aba8f150f80fe7b9c151a990e77d3f273a9
damarion989 <d.butlers282@gmail.com>
2026-08-30 20:21:01 -0500

    Implement SocialMediaBot as an Observer

--- SocialMediaBot.java
+++ SocialMediaBot.java
@@ -1,1 +1,12 @@
+public class SocialMediaBot implements Observer {
+    private GameTicker ticker;
 
+    public SocialMediaBot(GameTicker ticker) {
+        this.ticker = ticker;
+    }
+
+    @Override
+    public void update() {
+        System.out.println("TWEET: " + ticker.getLatestUpdate() + " #GameDay");
+    }
+}
